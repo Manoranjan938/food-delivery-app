fd8a520145bf953efaf5383efb2dde0ec18655e6
Manoranjan <sahoomanoranjan626@gmail.com>
2021-07-29 23:53:44 +0530

    Added restaurant bio header with restaurant image, address, rating and offer details

--- src/containers/RestaurantBio/index.jsx
+++ src/containers/RestaurantBio/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../components/Home/Header/Header'
+import RestaurantBioHeader from '../../components/Restaurant/RestaurantBioHeader'
 
 const RestaurantBio = () => {
     return (
         <>
             <Header />
+            <RestaurantBioHeader />
         </>
     )
 }
