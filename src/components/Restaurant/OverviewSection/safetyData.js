b9b1d38d42bf5ae7e763829f4b52a3b2d08b601a
Manoranjan <sahoomanoranjan626@gmail.com>
2021-08-01 19:56:20 +0530

    Updated the restaurant overview design and added some image files

---
+++ src/components/Restaurant/OverviewSection/safetyData.js
@@ -0,0 +1,21 @@
+import image1 from '../../../Images/kitchen-sanitization.png'
+import image2 from '../../../Images/hand-sanitization.png'
+import image3 from '../../../Images/temp-check.png'
+
+export const safetyData = [
+    {
+        image: image1,
+        mainTitle: "Restaurant Safety Measure",
+        subTitle: "Well Sanitized Kitchen"
+    },
+    {
+        image: image2,
+        mainTitle: "Restaurant Safety Measure",
+        subTitle: "Rider Hand Sanitization"
+    },
+    {
+        image: image3,
+        mainTitle: "Restaurant Safety Measure",
+        subTitle: "Daily Temp. Check"
+    }
+]
