7ba19f46c170d19d9d44d360a3087899bc974c78
Manoranjan <sahoomanoranjan626@gmail.com>
2021-08-04 23:30:47 +0530

    Added the food section design like list the all food design and added the food temporary cart section in all food section

--- src/components/Restaurant/SingleFood/index.jsx
+++ src/components/Restaurant/SingleFood/index.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
 
+import './singleFood.scss'
+
+import images from '../../../Images/fasoos.jpg'
+
 const SingleFood = () => {
     return (
-        <>
-            <h1>Single Food Component</h1>
-        </>
-    )
+      <>
+        <div className="single_food_container">
+          <div className="single_food_wrapper">
+            <div className="food_details_section">
+              <div className="food_type">
+                <i className="fa fa-stop-circle-o veg" />
+                <span className="bestseller">
+                  <i className="fa fa-star" />
+                  <p>Bestseller</p>
+                </span>
+              </div>
+              <div className="food_details">
+                <span>Crispy Veg Double Patty + Crispy Veg Double Patty</span>
+              </div>
+              <div className="food_price">
+                <span>
+                  <i className="fa fa-inr" />
+                  150 | <span className="food_offer">60% OFF | USE DEAL60</span>
+                </span>
+              </div>
+            </div>
+            <div className="food_image_and_add_section">
+              <img src={images} alt="" />
+              <div className="add_btn">
+                  <button>Add</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr />
+      </>
+    );
 }
 
 export default SingleFood
