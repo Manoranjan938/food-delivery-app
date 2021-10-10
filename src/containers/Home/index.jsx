7d38711fc1104f2c8a8e2e4f73d56a8c07abacc8
Manoranjan Sahoo <sahoomanoranjan626@gmail.com>
2021-10-10 18:39:46 +0530

    Added hero section design

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from 'components/Home/Header/Header'
+import Hero from 'components/Home/Hero/Hero';
 
 const Home = () => {
     return (
       <>
         <Header />
+        <Hero />
       </>
     );
 }
